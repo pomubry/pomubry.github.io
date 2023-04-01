51e4b985cde47cc4ed54110f40866a4cb7700df8
pomubry <btaduran30@protonmail.com>
2023-04-01 21:13:55 +0800

    remove emailjs dependency
       - send email form to a different api instead
       - add form validation using zod

--- components/Contact/ContactForm.tsx
+++ components/Contact/ContactForm.tsx
@@ -1,7 +1,9 @@
-import React, { useState } from "react";
+import React, { FormEventHandler, useState } from "react";
 import {
   Box,
   Button,
+  FormControl,
+  FormErrorMessage,
   Heading,
   Input,
   InputGroup,
@@ -13,7 +15,23 @@ import {
 } from "@chakra-ui/react";
 import { MdOutlineEmail, MdOutlineMessage, MdSend } from "react-icons/md";
 import { BiUser } from "react-icons/bi";
-import emailjs from "@emailjs/browser";
+import { bodySchema, bodyType } from "../../lib/utils";
+
+interface EmailRes {
+  title: string;
+  description: string;
+  status?: "info" | "warning" | "success" | "error" | "loading";
+  apiDescription: string;
+}
+
+type SubmitHandler = FormEventHandler<HTMLDivElement> &
+  FormEventHandler<HTMLFormElement>;
+
+const initialError = {
+  user_name: "",
+  user_email: "",
+  message: "",
+};
 
 const toastProps: UseToastOptions = {
   duration: 9000,
@@ -25,35 +43,70 @@ const color = "purple.500";
 
 const ContactForm = () => {
   const [isLoading, setIsLoading] = useState(false);
+  const [formError, setFormError] = useState<bodyType>(initialError);
   const toast = useToast();
   const bgColor = useColorModeValue("gray.100", "gray.900");
 
-  const handleSubmit = async (e: React.SyntheticEvent) => {
+  const handleSubmit: SubmitHandler = async (e) => {
     e.preventDefault();
     setIsLoading(true);
+    setFormError(initialError);
+
+    const formData = new FormData(e.currentTarget as HTMLFormElement);
+    const formDataValue = Object.fromEntries(formData.entries());
+    const parsedBody = bodySchema.safeParse(formDataValue);
+
+    if (!parsedBody.success) {
+      const fieldErrors = parsedBody.error.formErrors.fieldErrors;
+      const errors = Object.keys(fieldErrors).reduce(
+        (acc, cur) => {
+          const key = cur as keyof bodyType;
+          acc[key] = fieldErrors[key]![0];
+          return acc;
+        },
+        { ...formError }
+      );
+      setIsLoading(false);
+      return setFormError(errors);
+    }
 
     try {
-      await emailjs.sendForm(
-        "service_2cfy11v",
-        "template_d6g04ga",
-        e.target as HTMLFormElement,
-        "zdluO3hAdkIaOKCgi"
-      );
+      const res = await fetch("https://headlessani.vercel.app/api/email", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(parsedBody.data),
+      });
+      const data = (await res.json()) as EmailRes;
+      const { apiDescription, ...toastData } = data;
       toast({
-        title: "Success!",
-        description: "Thanks for your time. Your message has been sent!",
-        status: "success",
+        ...toastData,
         ...toastProps,
       });
-    } catch (err) {
-      const error = err as { text: string };
-      toast({
-        title: "Failed!",
-        description: "Sorry. Something went wrong with the message.",
-        status: "error",
-        ...toastProps,
-      });
-      console.error(error.text);
+    } catch (e) {
+      console.error(e);
+      if (e instanceof TypeError) {
+        toast({
+          ...toastProps,
+          title: e.name,
+          description: e.message,
+          status: "error",
+        });
+      } else if ((e as EmailRes)?.apiDescription?.length > 0) {
+        const { apiDescription, ...toastData } = e as EmailRes;
+        toast({
+          ...toastProps,
+          ...toastData,
+        });
+      } else {
+        toast({
+          ...toastProps,
+          title: "Error",
+          description: "Something went wrong",
+          status: "error",
+        });
+      }
     } finally {
       setIsLoading(false);
     }
@@ -78,46 +131,63 @@ const ContactForm = () => {
         Send me a message!
       </Heading>
 
-      <InputGroup borderColor={color}>
-        <InputLeftElement pointerEvents="none" color={color}>
-          <BiUser />
-        </InputLeftElement>
-        <Input
-          type="text"
-          name="user_name"
-          placeholder="Name"
-          focusBorderColor={color}
-          required
-        />
-      </InputGroup>
+      <FormControl isInvalid={!!formError.user_name.length}>
+        <InputGroup borderColor={color}>
+          <InputLeftElement pointerEvents="none" color={color}>
+            <BiUser />
+          </InputLeftElement>
+          <Input
+            type="text"
+            name="user_name"
+            placeholder="Name"
+            defaultValue={""}
+            focusBorderColor={color}
+            required
+          />
+        </InputGroup>
+        {!!formError.user_name.length && (
+          <FormErrorMessage>{formError.user_name}</FormErrorMessage>
+        )}
+      </FormControl>
 
-      <InputGroup borderColor={color}>
-        <InputLeftElement pointerEvents="none" color={color}>
-          <MdOutlineEmail />
-        </InputLeftElement>
-        <Input
-          type="email"
-          name="user_email"
-          placeholder="Email"
-          focusBorderColor={color}
-          required
-        />
-      </InputGroup>
+      <FormControl isInvalid={!!formError.user_email.length}>
+        <InputGroup borderColor={color}>
+          <InputLeftElement pointerEvents="none" color={color}>
+            <MdOutlineEmail />
+          </InputLeftElement>
+          <Input
+            type="email"
+            name="user_email"
+            defaultValue={""}
+            placeholder="Email"
+            focusBorderColor={color}
+            required
+          />
+        </InputGroup>
+        {!!formError.user_email.length && (
+          <FormErrorMessage>{formError.user_email}</FormErrorMessage>
+        )}
+      </FormControl>
 
-      <InputGroup borderColor={color}>
-        <InputLeftElement pointerEvents="none" color={color}>
-          <MdOutlineMessage />
-        </InputLeftElement>
-
-        <Textarea
-          name="message"
-          placeholder="Please enter your message"
-          focusBorderColor={color}
-          rows={5}
-          required
-          sx={{ textIndent: 25 }}
-        />
-      </InputGroup>
+      <FormControl isInvalid={!!formError.message.length}>
+        <InputGroup borderColor={color}>
+          <InputLeftElement pointerEvents="none" color={color}>
+            <MdOutlineMessage />
+          </InputLeftElement>
+          <Textarea
+            name="message"
+            placeholder="Please enter your message"
+            focusBorderColor={color}
+            rows={5}
+            required
+            defaultValue={""}
+            sx={{ textIndent: 25 }}
+          />
+        </InputGroup>
+        {!!formError.message.length && (
+          <FormErrorMessage>{formError.message}</FormErrorMessage>
+        )}
+      </FormControl>
 
       <Button
         type="submit"
---
+++ lib/utils.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+export const bodySchema = z.object({
+  user_name: z
+    .string()
+    .min(5, { message: "Username should be at least 5 characters long" }),
+  user_email: z
+    .string()
+    .min(1, { message: "Email should not be empty" })
+    .email({ message: "This is not a valid email" }),
+  message: z
+    .string()
+    .min(5, { message: "Message should be at least 5 characters long" }),
+});
+
+export type bodyType = z.infer<typeof bodySchema>;
