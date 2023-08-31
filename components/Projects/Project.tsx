26c7f4c96c3a3fd626f4789a14fead45aaf7f39b
pomubry <btaduran30@protonmail.com>
2023-08-31 20:40:28 +0800

    add dialog component for image

--- components/Projects/Project.tsx
+++ components/Projects/Project.tsx
@@ -1,5 +1,10 @@
+import * as Dialog from "@radix-ui/react-dialog";
+import * as VisuallyHidden from "@radix-ui/react-visually-hidden";
 import { BsGithub, BsLink } from "react-icons/bs";
+import { FaWindowClose } from "react-icons/fa";
 import type { IconType } from "react-icons";
+
+import styles from "./Project.module.css";
 
 interface ProjectProps {
   title: string;
@@ -40,14 +45,41 @@ const Project = ({ title, img, desc, github, site, index }: ProjectProps) => {
   return (
     <li className="bg-card my-10 grid grid-cols-1 items-center gap-5 rounded-lg p-8 shadow-xl md:grid-cols-2">
       {/* Image */}
-      <div
-        className={`order-1 overflow-hidden rounded-lg ${
-          isEven ? "md:order-1" : "md:order-2"
-        }`}
-      >
-        <picture>
-          <img src={img} alt={title} className="object-cover" />
-        </picture>
+      <div className={`order-1 ${isEven ? "md:order-1" : "md:order-2"}`}>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <button className="overflow-hidden rounded-lg">
+              <picture>
+                <img src={img} alt={title} className="object-cover" />
+              </picture>
+            </button>
+          </Dialog.Trigger>
+          <Dialog.Portal>
+            <Dialog.Overlay
+              className={`${styles.DialogOverlay} fixed inset-0 bg-gray-900/90`}
+            />
+            <Dialog.Content
+              className={`${styles.DialogContent} fixed left-[50%] top-[50%] max-h-[85vh] w-[80vw] translate-x-[-50%] translate-y-[-50%] overflow-hidden rounded-lg text-gray-900 focus:outline-none dark:text-gray-100`}
+            >
+              <VisuallyHidden.Root>
+                <Dialog.Title>{title}</Dialog.Title>
+              </VisuallyHidden.Root>
+
+              <picture>
+                <img src={img} alt={title} className="object-cover" />
+              </picture>
+
+              <Dialog.Close asChild>
+                <button
+                  className="absolute right-[10px] top-[10px] inline-flex h-7 w-7 appearance-none bg-gray-900 text-gray-100"
+                  aria-label={`Close modal for ${title}`}
+                >
+                  <FaWindowClose className="h-full w-full" />
+                </button>
+              </Dialog.Close>
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
       </div>
 
       {/* Description */}
